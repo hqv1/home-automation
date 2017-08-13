5378e9e91079fd592186290cd8679e108cc59500
hqv1 <v.hoang@outlook.com>
2017-08-13 15:26:51 -0700

    Created the solution and project. Got health check controller up. Added Swagger. Added global logging with Serilog.

---
+++ Thermostat/Thermostat.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hqv.Thermostat.Api.Controllers
+{
+    [Route("api/health")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        [HttpHead]
+        public IActionResult Get()
+        {
+            return Ok("Normal");
+        }
+    }
+}
