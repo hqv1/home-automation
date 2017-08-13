e4ab3da62f26e396a9b5dd9b86834e22c598b1c9
hqv1 <v.hoang@outlook.com>
2017-08-13 15:26:51 -0700

    Created the solution and project. Got health check controller up. Added Swagger. Added global logging with Serilog.

---
+++ Thermostat/Thermostat.Api/Startup.cs
@@ -0,0 +1,85 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Serilog;
+using Serilog.Formatting.Json;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Hqv.Thermostat.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File("log\\global_logs.json")
+                .CreateLogger();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                // Add MVC
+                .AddMvc(action =>
+                {
+                    // Return 406 if we cannot generate the response in the requested format 
+                    // Can output in XML
+                    // Can accept XML 
+                    action.ReturnHttpNotAcceptable = true;
+                    action.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    action.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+                })
+                // Shaping the return result will make the properties uppercase. This JSON option
+                //will make it lower case. Reason is the returned object is an ExpandoObject which uses
+                //a dictionary in the background. The default ContractResolver does not correctly work with
+                //a dictionary.
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                })
+                // Use Fluent Validation for validation instead of the built-in validation. I like to 
+                //see validation in one place, rather than in attributes and in code. 
+                .AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<Startup>())
+                ;
+            ;
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Web Api Pattern Core", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddSerilog();            
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(); // Use MVC            
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            Log.Logger.Error("Hi");
+            Log.Logger.Error("Hello {Person}", new {name = "Bob"});
+        }
+    }
+}
